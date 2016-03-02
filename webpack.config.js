678347b6d45df7be41ffe01c1832efe84179ddf8
Arthur Flachs <arthur.flachs@gmail.com>
2016-03-02 23:53:24 +0100

    Installs Babel and virtual-dom

--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,11 @@ module.exports = {
     path: path.resolve('dist'),
     filename: 'bundle.js',
     publicPath: '/static/'
+  },
+
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: "babel", exclude: /node_modules/ },
+    ]
   }
 }
