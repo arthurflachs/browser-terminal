3902279fa793c37a5f60a850cc5f9b3f9863a0a0
Arthur Flachs <arthur.flachs@gmail.com>
2016-03-04 13:07:58 +0100

    Imports babel-register for ES6 support

--- commands-server/server.js
+++ commands-server/server.js
@@ -1,8 +1,10 @@
 // Server accepts command requests in form of an array. It pipes the command to a specified server (ssh2 or process.spawn). It returns the text to the client
 
-var express = require('express')
+require("babel-register")
 
-var app = express()
+const express = require('express')
+
+const app = express()
 const port = 8081
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
